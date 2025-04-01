e4e697b4b7fdce812bab896104ae6b230d3b2263
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-01 13:51:12 -0300

    feat: adding configuration to use dependency injection and create services folder to use autoMapper and passwordEncrypter

---
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests.User;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Application.Services.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            RequestToDomain();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<RequestRegisterUserJson, User>()
+                .ForMember(dest => dest.Password, option => option.Ignore());
+        }
+        private void DomainToResponse()
+        {
+
+        }
+    }
+}
