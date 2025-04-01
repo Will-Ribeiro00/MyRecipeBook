469de4cdaaa299f62da50a435429ec5a549c3014
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-01 14:54:41 -0300

    feat: adding the  use case to register an user

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Communication.Requests.User;
+using MyRecipeBook.Communication.Responses.User;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public interface IRegisterUserUseCase
+    {
+        public Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request);
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests.User;
+using MyRecipeBook.Communication.Responses.User;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Exception;
+using MyRecipeBook.Exception.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase : IRegisterUserUseCase
+    {
+        private readonly IUserReadOnlyRepository _readOnlyRepository;
+        private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly PasswordEncrypter _passwordEncrypter;
+
+        public RegisterUserUseCase(IUserReadOnlyRepository readOnly, IUserWriteOnlyRepository writeOnly, IMapper mapper, PasswordEncrypter passwordEncrypter, IUnitOfWork unitOfWork)
+        {
+            _readOnlyRepository = readOnly;
+            _writeOnlyRepository = writeOnly;
+            _mapper = mapper;
+            _passwordEncrypter = passwordEncrypter;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
+        {
+            await Validate(request);
+
+            var user = _mapper.Map<Domain.Entities.User>(request);
+            user.Password = _passwordEncrypter.Encrypt(request.Password);
+
+
+            await _writeOnlyRepository.Add(user);
+            await _unitOfWork.Commit();
+            
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name
+            };
+        }
+
+        private async Task Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+
+            var result = validator.Validate(request);
+
+            var emailExist = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
+            if(emailExist)
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesExceptions.EMAIL_ALREADY_REGISTERED));
+
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(erro => erro.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+    }
+}
