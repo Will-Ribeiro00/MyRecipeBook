a24f2db51a0b98d21f55be4234dbe8edb0cda3b4
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-01 13:47:15 -0300

    feat: adding Database Context, user repository and unit of work to do commits

---
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext(services, configuration);
+            AddUnitOfWork(services);
+            AddRepositories(services);
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 39));
+
+            services.AddDbContext<MyRecipeBookDbContext>(options =>
+            {
+                options.UseMySql(connectionString, serverVersion);
+            });
+        }
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        }
+        private static void AddUnitOfWork(IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
