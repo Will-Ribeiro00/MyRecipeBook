e84fe9cb92943139951bf1996b82fa49a3ae8188
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-04 10:57:45 -0300

    fix: Created migrate version 2 to adjust bug on database where the application can't find the user table.

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
@@ -2,6 +2,7 @@
 {
     public abstract class DatabaseVersions
     {
-        public const int TABLE_USER = 1; 
+        public const int TABLE_USER = 1;
+        public const int ADJUSTE_TABLE_USER = 2;
     }
 }
