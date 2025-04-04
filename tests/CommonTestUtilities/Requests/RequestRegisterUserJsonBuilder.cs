71bf767140614538d1f96d41c44f129cbc87e59a
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-04 11:37:59 -0300

    feat: add the CommonTestUtilities library for use in other tests of the application

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests.User;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterUserJsonBuilder
+    {
+        public static RequestRegisterUserJson Build()
+        {
+            return new Faker<RequestRegisterUserJson>()
+                .RuleFor(user => user.Name, faker => faker.Person.FirstName)
+                .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+                .RuleFor(user => user.Password, faker => faker.Internet.Password(length: 8, prefix: "!Aa1"));
+        }
+    }
+}
