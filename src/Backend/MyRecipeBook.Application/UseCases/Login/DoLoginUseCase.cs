fc5b55c19b7f2a2700db49e670842de7994a6e67
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-08 17:58:38 -0300

    feat: added use case to validate a user login

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Login/DoLoginUseCase.cs
@@ -0,0 +1,31 @@
+﻿using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests.Login;
+using MyRecipeBook.Communication.Responses.User;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Exception.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.Login
+{
+    public class DoLoginUseCase : IDoLoginUseCase
+    {
+        private readonly IUserReadOnlyRepository _repository;
+        private readonly PasswordEncrypter _passwordEncrypter;
+
+        public DoLoginUseCase(IUserReadOnlyRepository repository, PasswordEncrypter passwordEncrypter)
+        {
+            _repository = repository;
+            _passwordEncrypter = passwordEncrypter;
+        }
+        public async Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request)
+        {
+            var encrypterPassword = _passwordEncrypter.Encrypt(request.Password);
+
+            var user = await _repository.GetByEmailAndPassword(request.Email, encrypterPassword) ?? throw new InvalidLoginException();
+
+            return  new ResponseRegisteredUserJson
+            {
+                Name = user.Name
+            };
+        }
+    }
+}
