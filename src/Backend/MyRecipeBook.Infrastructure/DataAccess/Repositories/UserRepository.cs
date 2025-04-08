04dc232353899b5509de84e673300fbc9d298f11
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-08 15:50:09 -0300

    feat: added method to retrieve an user by email and password in IUserReadOnlyRepository and implemented in UserRepository

--- src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -13,5 +13,10 @@ namespace MyRecipeBook.Infrastructure.DataAccess.Repositories
         public async Task Add(User user) => await _context.Users.AddAsync(user);
 
         public async Task<bool> ExistActiveUserWithEmail(string email) => await _context.Users.AnyAsync(user => user.Email.Equals(email) && user.Active);
+
+        public async Task<User?> GetByEmailAndPassword(string email, string password)
+        {
+            return await _context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Active && user.Email.Equals(email) && user.Password.Equals(password));
+        }
     }
 }
