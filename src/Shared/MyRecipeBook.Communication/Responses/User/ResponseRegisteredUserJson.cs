40adea4452207bcdaecf5cc759162728008e6b4e
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-10 14:23:50 -0300

    feat: created responseTokenJson

--- src/Shared/MyRecipeBook.Communication/Responses/User/ResponseRegisteredUserJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/User/ResponseRegisteredUserJson.cs
@@ -1,7 +1,10 @@
-﻿namespace MyRecipeBook.Communication.Responses.User
+﻿using MyRecipeBook.Communication.Responses.Tokens;
+
+namespace MyRecipeBook.Communication.Responses.User
 {
     public class ResponseRegisteredUserJson
     {
         public string Name { get; set; } = string.Empty;
+        public ResponseTokensJson Tokens { get; set; } = default!;
     }
 }
