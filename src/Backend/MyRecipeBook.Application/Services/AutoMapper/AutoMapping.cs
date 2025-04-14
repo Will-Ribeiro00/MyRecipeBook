2e0e499817801d52bacdc456a8c87edd9711fd28
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-14 12:11:20 -0300

    feat: created auto mapper configuration to mapping an entity user to a response UserProfile

--- src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MyRecipeBook.Communication.Requests.User;
+using MyRecipeBook.Communication.Responses.User;
 using MyRecipeBook.Domain.Entities;
 
 namespace MyRecipeBook.Application.Services.AutoMapper
@@ -9,12 +10,18 @@ namespace MyRecipeBook.Application.Services.AutoMapper
         public AutoMapping()
         {
             RequestToDomain();
+            DomainToResponse();
         }
 
         private void RequestToDomain()
         {
             CreateMap<RequestRegisterUserJson, User>()
                 .ForMember(dest => dest.Password, option => option.Ignore());
+        }
+
+        private void DomainToResponse()
+        {
+            CreateMap<User, ResponseUserProfileJson>();
         }
     }
 }
