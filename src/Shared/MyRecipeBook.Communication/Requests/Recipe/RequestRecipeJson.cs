8447846d2fcd8b2193bf8bba40b3a6f7de05e30d
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-22 09:48:36 -0300

    feat: copied enums by Domain to Communication and added new request class to recipe

---
+++ src/Shared/MyRecipeBook.Communication/Requests/Recipe/RequestRecipeJson.cs
@@ -0,0 +1,14 @@
+﻿using MyRecipeBook.Communication.Enums;
+
+namespace MyRecipeBook.Communication.Requests.Recipe
+{
+    public class RequestRecipeJson
+    {
+        public string Title { get; set; } = string.Empty;
+        public CookingTime? CookingTime { get; set; }
+        public Difficulty? Difficulty { get; set; }
+        public IList<string> Ingredients { get; set; } = [];
+        public IList<RequestIntructionJson> Instructions { get; set; } = [];
+        public IList<DishType> DishTypes { get; set; } = [];
+    }
+}
