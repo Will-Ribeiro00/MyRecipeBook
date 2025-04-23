7fd03dc6816cd8815dacc7cdf856564c9a88b4bd
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-23 18:56:59 -0300

    tests: adjusted mapper builder to new constructor

---
+++ tests/CommonTestUtilities/IdEncryption/IdEncrypterBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Sqids;
+
+namespace CommonTestUtilities.IdEncryption
+{
+    public class IdEncrypterBuilder
+    {
+        public static SqidsEncoder<long> Build()
+        {
+            return new SqidsEncoder<long>(new()
+            {
+                MinLength = 3,
+                Alphabet = "lKBNsyI2Xa0OY6xLnRgdPfp47STDerFoiVEZwmWhkjQqvM1cCbJHG9Az5tu8U3"
+            });
+        }
+    }
+}
