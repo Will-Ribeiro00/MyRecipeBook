87599acc1db9d7ddb65c8dd00ddd1b8d9e8d9de8
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-28 08:21:54 -0300

    feat: added response class and request class to filter recipes

---
+++ src/Shared/MyRecipeBook.Communication/Responses/Recipe/ResponseRecipesJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Responses.Recipe
+{
+    public class ResponseRecipesJson
+    {
+        public IList<ResponseShortRecipeJson> Recipes { get; set; } = [];
+    }
+}
