045b916696670a2d2a1a7603639ce08862e816b9
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-04-28 08:24:42 -0300

    feat: added IRecipeReadOnly repository to recipes and added DTO to filter recipes

---
+++ src/Backend/MyRecipeBook.Domain/Dto/Recipe/FilterRecipesDto.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Dto.Recipe
+{
+    public record FilterRecipesDto
+    {
+        public string? RecipeTitle_Ingredient { get; set; }
+        public IList<CookingTime> CookingTimes { get; set; } = [];
+        public IList<Difficulty> Difficulties { get; set; } = [];
+        public IList<DishType> DishTypes { get; set; } = [];
+    }
+}
