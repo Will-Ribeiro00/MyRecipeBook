475c2bb056fad18ebaf2671683e7a29dcddc9e1f
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-05-02 09:24:54 -0300

    feat: added use case to delete a recipe

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Delete/DeleteRecipeUseCase.cs
@@ -0,0 +1,36 @@
+﻿using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Exception;
+using MyRecipeBook.Exception.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Delete
+{
+    public class DeleteRecipeUseCase : IDeleteRecipeUseCase
+    {
+        private readonly IRecipeWriteOnlyRepository _writeRepository;
+        private readonly IRecipeReadOnlyRepository _readRepository;
+        private readonly ILoggedUser _loggedUser;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteRecipeUseCase(IRecipeWriteOnlyRepository writeRepository, IRecipeReadOnlyRepository readRepository, ILoggedUser loggedUser, IUnitOfWork unitOfWork)
+        {
+            _writeRepository = writeRepository;
+            _readRepository = readRepository;
+            _loggedUser = loggedUser;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(long recipeId)
+        {
+            var loggedUser = await _loggedUser.User();
+
+            var recipe = await _readRepository.RecipeExist(loggedUser, recipeId);
+            if (!recipe)
+                throw new NotFoundException(ResourceMessagesExceptions.RECIPE_NOT_FOUND);
+
+            await _writeRepository.Delete(recipeId);
+            await _unitOfWork.Commit();
+        }
+    }
+}
