170f90a136735d41a8045f1f8acb9a8f00e3a06d
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-05-02 13:24:20 -0300

    test: added use case's tests and integration's test to get dashboard

---
+++ tests/UseCaseTest/Dashboard/Get/GetDashboardUseCaseTest.cs
@@ -0,0 +1,44 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using MyRecipeBook.Application.UseCases.Dashboard.Get;
+using Shouldly;
+
+namespace UseCaseTest.Dashboard.Get
+{
+    public class GetDashboardUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            // Arrangge
+            (var user, var _) = UserBuilder.Build();
+            var recipes = RecipeBuilder.Collection(user);
+            var useCase = CreateUseCase(user, recipes);
+
+            // Act
+            var result = await useCase.Execute();
+
+            // Asserts
+            result.ShouldNotBeNull();
+            result.Recipes.Count.ShouldBeGreaterThan(0);
+            result.Recipes.Select(r => r.Id).ToList().Distinct().Count().ShouldBe(result.Recipes.Count);
+            foreach(var recipe in result.Recipes)
+            {
+                recipe.Id.ShouldNotBeNullOrWhiteSpace();
+                recipe.Title.ShouldNotBeNullOrWhiteSpace();
+                recipe.AmountIngredients.ShouldBeGreaterThan(0);
+            }
+        }
+
+        private static GetDashboardUseCase CreateUseCase(MyRecipeBook.Domain.Entities.User user, IList<MyRecipeBook.Domain.Entities.Recipe> recipes)
+        {
+            var mapper = MapperBuilder.Build();
+            var loggedUser = LoggedUserBuilder.Build(user);
+            var repository = new RecipeReadOnlyRepositoryBuilder().GetForDashboard(user, recipes).Build();
+
+            return new GetDashboardUseCase(repository, mapper, loggedUser);
+        }
+    }
+}
