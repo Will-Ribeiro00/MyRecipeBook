31b2264683a70297e4c06e74219266a97190f10e
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-05-22 14:26:56 -0300

    test: tests added to validate the integration to the endpoint to generate recipes

---
+++ tests/WebApi.Test/Recipe/Generate/GenerateRecipeInvalidTokenTest.cs
@@ -0,0 +1,82 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using MyRecipeBook.Communication.Requests.Recipe;
+using MyRecipeBook.Exception;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Recipe.Generate
+{
+    public class GenerateRecipeInvalidTokenTest : MyRecipeBookClassFixture
+    {
+        private const string METHOD = "recipe/generate";
+
+        public GenerateRecipeInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory) { }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task ErrorTokenInvalid(string culture)
+        {
+            // Arrange and Act
+            var request = RequestGenerateRecipeJsonBuilder.Build();
+            var response = await DoPost(method: METHOD, request: request, token: "tokenInvalido", culture: culture);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+            var expectedMessage = ResourceMessagesExceptions.ResourceManager.GetString("USER_WITHOUT_PERMISSION_ACCESS_RESOURCE", new CultureInfo(culture));
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+            errors.ShouldHaveSingleItem();
+            errors.ShouldContain(e => e.GetString()!.Equals(expectedMessage));
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task ErrorWithoutToken(string culture)
+        {
+            // Arrange and Act
+            var request = RequestGenerateRecipeJsonBuilder.Build();
+            var response = await DoPost(method: METHOD, request: request, token: string.Empty, culture: culture);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+            var expectedMessage = ResourceMessagesExceptions.ResourceManager.GetString("WITHOUT_TOKEN", new CultureInfo(culture));
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+            errors.ShouldHaveSingleItem();
+            errors.ShouldContain(e => e.GetString()!.Equals(expectedMessage));
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task ErrorTokenWithUserNotFound(string culture)
+        {
+            // Arrange
+            var request = RequestGenerateRecipeJsonBuilder.Build();
+            var token = JwtTokenGeneratorBuilder.Build().Generate(Guid.NewGuid());
+
+            // Act
+            var response = await DoPost(method: METHOD, request: request, token: token, culture: culture);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+            var expectedMessage = ResourceMessagesExceptions.ResourceManager.GetString("USER_WITHOUT_PERMISSION_ACCESS_RESOURCE", new CultureInfo(culture));
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+            errors.ShouldHaveSingleItem();
+            errors.ShouldContain(e => e.GetString()!.Equals(expectedMessage));
+        }
+    }
+}
