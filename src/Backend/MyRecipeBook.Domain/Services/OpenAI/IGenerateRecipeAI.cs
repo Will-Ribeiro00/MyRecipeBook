d6bb2b93baff1532cf13f1a1f37fabc1694bbf28
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-05-22 11:47:50 -0300

    feat: created IGenerateRecipeIA Interface and the resources to send a generate message to chatGPT

---
+++ src/Backend/MyRecipeBook.Domain/Services/OpenAI/IGenerateRecipeAI.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Domain.Dto.Generate;
+
+namespace MyRecipeBook.Domain.Services.OpenAI
+{
+    public interface IGenerateRecipeAI
+    {
+        Task<GeneratedRecipeDto> Generate(IList<string> ingredients);
+    }
+}
