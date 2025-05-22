d0bc0def300a56e247fb58e316160690a6f28e62
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-05-22 11:36:42 -0300

    feat: added request class and response class to generate a recipe using chatGPT

---
+++ src/Shared/MyRecipeBook.Communication/Responses/Recipe/ResponseGeneratedInstructionJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Communication.Responses.Recipe
+{
+    public class ResponseGeneratedInstructionJson
+    {
+        public int Step { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
