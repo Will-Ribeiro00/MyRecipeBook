395e6ec37d938d25bf623915b2d798dee93ac7ce
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-05-29 13:51:43 -0300

    feat: added image identifier property in the Recipe entity and its respective migration in the database

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000005.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.IMAGES_FOR_RECIPES, "Add collumn on recipe table to save images")]
+    public class Version0000005 : VersionBase
+    {
+        public override void Up()
+        {
+            Alter.Table("Recipes")
+                .AddColumn("ImageIdentifier").AsString().Nullable();
+        }
+    }
+}
