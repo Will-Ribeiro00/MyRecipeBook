00e86e9a9cf9a262a8cea47911d14d5f3f5a386e
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-08-01 11:51:56 -0300

    test: adjusted configuration classes to succeed in all tests with using a new "associate image with recipe" feature

--- tests/WebApi.Test/MyRecipeBookClassFixture.cs
+++ tests/WebApi.Test/MyRecipeBookClassFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Collections;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace WebApi.Test
@@ -8,6 +9,30 @@ namespace WebApi.Test
         private readonly HttpClient _httpClient;
         public MyRecipeBookClassFixture(CustomWebApplicationFactory factory) => _httpClient = factory.CreateClient();
 
+        protected async Task<HttpResponseMessage> DoPostFormData(string method, object request, string token = "", string culture = "en")
+        {
+            ChangeRequestCulture(culture);
+            AuthorizeRequest(token);
+
+            var multipartContent = new MultipartFormDataContent();
+
+            var requestProperties = request.GetType().GetProperties();
+
+            foreach (var property in requestProperties)
+            {
+                var propertyValue = property.GetValue(request);
+
+                if (string.IsNullOrWhiteSpace(propertyValue?.ToString()))
+                    continue;
+
+                if (propertyValue is IList list)
+                    AddListToMultipartContent(multipartContent, property.Name, list);
+                else
+                    multipartContent.Add(new StringContent(propertyValue.ToString()!), property.Name);
+            }
+
+            return await _httpClient.PostAsync(method, multipartContent);
+        }
         protected async Task<HttpResponseMessage> DoPost(string method, object request, string token = "", string culture = "en")
         {
             ChangeRequestCulture(culture);
@@ -37,6 +62,7 @@ namespace WebApi.Test
             return await _httpClient.DeleteAsync(method);
         }
 
+
         private void AuthorizeRequest(string token)
         {
             if (string.IsNullOrWhiteSpace(token)) return;
@@ -50,6 +76,41 @@ namespace WebApi.Test
                 _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
 
             _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+        }
+
+        private static void AddListToMultipartContent(MultipartFormDataContent multipartContent, string propertyName, IList list)
+        {
+            var itemType = list.GetType().GetGenericArguments().Single();
+
+            if (itemType.IsClass && itemType != typeof(string))
+            {
+                AddClassListToToMultipartContent(multipartContent, propertyName, list);
+            }
+            else
+            {
+                foreach (var item in list)
+                {
+                    multipartContent.Add(new StringContent(item.ToString()!), propertyName);
+                }
+            }
+        }
+
+        private static void AddClassListToToMultipartContent(MultipartFormDataContent multipartContent, string propertyName, IList list)
+        {
+            var index = 0;
+
+            foreach (var item in list)
+            {
+                var classPropertiesInfo = item.GetType().GetProperties().ToList();
+
+                foreach (var prop in classPropertiesInfo)
+                {
+                    var value  = prop.GetValue(item, null);
+                    multipartContent.Add(new StringContent(value!.ToString()!), $"{propertyName}[{index}][{prop.Name}]");
+                }
+
+                index++;
+            }
         }
     }
 }
