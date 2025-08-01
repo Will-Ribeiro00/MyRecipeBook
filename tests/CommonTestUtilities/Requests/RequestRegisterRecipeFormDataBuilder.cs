8667feaa34808895dd7f0680ff7727a728a22c84
Williansberg Silva <william.ribeiro1403@gmail.com>
2025-08-01 13:17:01 -0300

    test: added all commom classes and files to succeed in all tests with using a new "associate image with recipe" feature

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterRecipeFormDataBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Microsoft.AspNetCore.Http;
+using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Communication.Requests.Recipe;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterRecipeFormDataBuilder
+    {
+        public static RequestRegisterRecipeFormData Build(IFormFile? formFile = null)
+        {
+            var step = 1;
+
+            return new Faker<RequestRegisterRecipeFormData>()
+                .RuleFor(r => r.Image, _ => formFile)
+                .RuleFor(r => r.Title, f => f.Lorem.Word())
+                .RuleFor(r => r.CookingTime, f => f.PickRandom<CookingTime>())
+                .RuleFor(r => r.Difficulty, f => f.PickRandom<Difficulty>())
+                .RuleFor(r => r.Ingredients, f => f.Make(3, () => f.Commerce.ProductName()))
+                .RuleFor(r => r.DishTypes, f => f.Make(3, () => f.PickRandom<DishType>()))
+                .RuleFor(r => r.Instructions, f => f.Make(3, () => new RequestIntructionJson
+                {
+                    Step = step++,
+                    Description = f.Lorem.Paragraph()
+                }));
+        }
+    }
+}
